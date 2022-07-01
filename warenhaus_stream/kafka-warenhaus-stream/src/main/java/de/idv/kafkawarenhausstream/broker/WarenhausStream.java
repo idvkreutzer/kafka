17ff2315547deb3f78d243cb751d895884925e59
Maximilian Kreutzer <mkre@idv-ag.de>
2022-07-01 12:43:32 +0200

    application.yml + beschreibung

--- warenhaus_stream/kafka-warenhaus-stream/src/main/java/de/idv/kafkawarenhausstream/broker/WarenhausStream.java
+++ warenhaus_stream/kafka-warenhaus-stream/src/main/java/de/idv/kafkawarenhausstream/broker/WarenhausStream.java
@@ -5,4 +5,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class WarenhausStream {
 
+	// TODO hier Stream einfuegen. An der Configuration Annotation kannst Du schon sehen, dass wir ein Bean benötigen. Also definiere eine Bean mit dem RueckgabeParameter KStream
+	// Schaue dir den KStream an ;). 
+	// Zur Hilfe, Inputparameter wird ein org.apache.kafka.streams.StreamsBuilder sein. Schaue dir den Streamsbuilder an.
 }
